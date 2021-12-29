c1e31846579cee5813094d4c6a180659ff80537c
babugarnaik <39863058+babugarnaik@users.noreply.github.com>
2021-12-30 00:20:46 +0530

    ifelsepractice3

---
+++ src/com/practice/IflesePractice3.java
@@ -0,0 +1,16 @@
+package com.practice;
+
+	public class IflesePractice3 {
+	  public static void main(String[] arg) {
+		  int num1=23;
+		  int num2=67;
+		  int num3=98;
+		  if(num1>num2 && num2<98) {
+		  System.out.println("greatest num is:"+num2); 	  
+		  
+		  }else {
+	  	  System.out.println("greatest num is:"+num3);
+		  }  	  
+}
+
+}
