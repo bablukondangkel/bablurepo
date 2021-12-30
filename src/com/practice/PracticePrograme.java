52aacf4113f8957874db2a02576e9113e02e489d
babugarnaik <39863058+babugarnaik@users.noreply.github.com>
2021-12-30 11:07:14 +0530

    Weekpro

---
+++ src/com/practice/PracticePrograme.java
@@ -0,0 +1,29 @@
+package com.practice;
+
+   public class PracticePrograme {
+	  public static void main(String[] arg) {
+		  int a=5;
+		  if(a==1) {
+			  System.out.println("1:sunday");
+		  }
+		  else if(a==2) {
+			  System.out.println("2:monday");
+		  }
+		  else if(a==3) {
+			  System.out.println("3:tuesday");
+		  }
+		  else if(a==4) {
+			  System.out.println("4:wensday");
+		  }
+		  else if(a==5) {
+			  System.out.println("5:thursday");
+		  }
+		  else if(a==6) {
+			  System.out.println("6:friday");
+		  }
+		  else {
+			  System.out.println("7:saturday");
+		  }		  
+		  
+	  }
+}
