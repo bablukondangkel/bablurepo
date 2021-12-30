50cfb43d1267dca1c99c331ec3bb00326425960a
babugarnaik <39863058+babugarnaik@users.noreply.github.com>
2021-12-30 13:52:47 +0530

    Profit

--- src/com/practice/GreatestPrograme.java
+++ src/com/practice/GreatestPrograme.java
@@ -2,13 +2,13 @@ package com.practice;
 
 	public class GreatestPrograme {
 	public static void main(String[] arg) {
-		int num1=23;
+		int num1=95;
 		int num2=45;
 		int num3=78;
-		if(num1>num2 && num2>num3) {
+		if(num1>num2 && num1>num3) {
 			System.out.println("greatest number is:"+num1);
 		}
-		if(num1<num2 && num2>num3) {
+		else if(num2>num3) {
 			System.out.println("greatest number is:"+num2);
 			
 		}else {
---
+++ src/com/practice/SalePrograme.java
@@ -0,0 +1,20 @@
+package com.practice;
+
+	public class SalePrograme {
+		public static void main(String[] arg) {
+			int costprice=100;
+			int kg=5;
+			int saleprice=120;
+			int totalCost = kg*costprice; 
+			int totalSale=kg*saleprice;
+			if(totalSale > totalCost) {
+				int profit=totalSale-totalCost; 
+				System.out.println("profit:"+profit);
+			}else{
+				int loss = totalCost - totalSale;
+				System.out.println("loss:"+loss);
+			}
+		
+		}
+	}
+
