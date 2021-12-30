d44afee3b09a8ac27f6d354f5613c65fcb6badea
babugarnaik <39863058+babugarnaik@users.noreply.github.com>
2021-12-30 11:20:07 +0530

    2number

---
+++ src/com/practice/GreatestPrograme.java
@@ -0,0 +1,16 @@
+package com.practice;
+
+public class GreatestPrograme {
+	public static void main(String[] arg) {
+		int num1=23;
+		int num2=45;
+		int num3=78;
+		if(num1>num2 && num2<num3) {
+			System.out.println("greatest number is:"+num2);
+		}else {
+			System.out.println("greatest number is:"+num3);
+			
+		}
+	}
+
+}
